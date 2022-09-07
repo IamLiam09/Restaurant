c65f2b772bc838e9d09768500ddc61a68fbd3ac3
liams <princewill835@gmail.com>
2022-09-06 20:09:26 -0700

    70% done on mobile responsiveness

--- src/index.js
+++ src/index.js
@@ -4,7 +4,6 @@ import chicken from "./assets/chicken.png";
 import  ziglines from "./assets/ziglines.jpg";
 import ziglines2 from "./assets/ziglines2.png";
 import created from "./assets/createdzig.png";
-
 const img = document.getElementById("plate")
 const bg = document.getElementById("bgimage")
 const ziglinesimg = document.getElementById("zigline__1")
@@ -25,11 +24,25 @@ ziglines4img.src = ziglines2
 var boo = false
 hamburger.addEventListener("click", open)
 window.addEventListener("scroll", standout)
+// EventListner for the ripple effect for the links
+buttons.addEventListener("click", addripples(e))
 content.style.height = "100vh"
+const buttons = document.querySelectorAll("a")
 // a function to add a few syle to make the header stand out
 function standout(){
     let header = document.querySelector("header");
-    header.classList.toogle("extra", window.scrollY > 0)
+    header.classList.toggle("extra", window.scrollY > 0)
+}
+function addripples(e){
+    let x = e.clientX - e.target.offsetLeft;
+    let y = e.clientY - e.target.offsetright;
+    let ripples = documeent.createElement("span")
+    ripples.style.left = x + "px"
+    ripples.style.top = y + "px"
+    this.appendChild(ripples)
+    setTimeout(() => {
+        ripples.remove()
+    }, 1000)
 }
 function checker(){
     if(content.style.height === "100vh"){
